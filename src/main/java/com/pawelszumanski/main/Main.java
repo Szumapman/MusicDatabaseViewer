5ecdc693ffb8b85d41b78ec62fa68d6eaa958660
Paweł Szumański <pawel.szumanski@outlook.com>
2018-07-26 16:56:37 +0200

    Dodanie internacjonalizacji(utworzenie Resource Bundles) w nowym katalogu bundles i uzupełnienie o plik domyślny (angielski) i pl. Utworzenie katalogu icons w resources i dodanie ikony do głównego okna.

--- src/main/java/com/pawelszumanski/main/Main.java
+++ src/main/java/com/pawelszumanski/main/Main.java
@@ -3,8 +3,11 @@ package com.pawelszumanski.main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -15,10 +18,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MainWindow.fxml"));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
         BorderPane pane = loader.load();
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
-//        primaryStage.setTitle("Music Database Viewer");
+        primaryStage.getIcons().add(new Image(this.getClass().getResource("/icons/music-base-16.png").toString()));
+        primaryStage.getIcons().add(new Image(this.getClass().getResource("/icons/music-base-32.png").toString()));
+        primaryStage.setTitle(bundle.getString("title.app"));
         primaryStage.show();
     }
 }
