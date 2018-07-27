a01d97b1dd704927f63b71e707a7bf0e0eac4eff
Paweł Szumański <pawel.szumanski@outlook.com>
2018-07-27 15:13:46 +0200

    Dokończenie refaktoryzacji klasy Main.

--- src/main/java/com/pawelszumanski/main/Main.java
+++ src/main/java/com/pawelszumanski/main/Main.java
@@ -7,13 +7,10 @@ package com.pawelszumanski.main;
 import com.pawelszumanski.utils.DialogsUtils;
 import com.pawelszumanski.utils.FxmlUtils;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.util.ResourceBundle;
 
 import static com.pawelszumanski.utils.PathUtils.ICONS_MUSIC_BASE_32_PNG;
 import static com.pawelszumanski.utils.PathUtils.MAIN_WINDOW_FXML;
@@ -26,15 +23,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = FxmlUtils.getLoader(MAIN_WINDOW_FXML);
-        ResourceBundle bundle = FxmlUtils.getResourceBundle();
-        loader.setResources(bundle);
-        BorderPane pane = loader.load();
+        Pane pane = FxmlUtils.fxmlLoader(MAIN_WINDOW_FXML);
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
 //        primaryStage.getIcons().add(new Image(this.getClass().getResource(ICONS_MUSIC_BASE_16_PNG).toString()));
         primaryStage.getIcons().add(new Image(this.getClass().getResource(ICONS_MUSIC_BASE_32_PNG).toString()));
-        primaryStage.setTitle(bundle.getString("title.app"));
+        primaryStage.setTitle(FxmlUtils.getResourceBundle().getString("title.app"));
         primaryStage.setOnCloseRequest(event -> {
             if(!DialogsUtils.closeAppConfirmationDialog()){
                 event.consume();
